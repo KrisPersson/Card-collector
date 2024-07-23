8a36c9919be065dc67707956550675de83583b76
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-07-23 23:06:51 +0200

    update

--- back-end/JSONchecklists/formatted.js
+++ back-end/JSONchecklists/formatted.js
@@ -1,15 +1,35 @@
 const raw = `
 
-1		 									Pavel Bure	 									Vancouver Canucks
-2		 									Alexandre Daigle	 									Ottawa Senators
-3		 									Sergei Fedorov	 									Detroit Red Wings
-4		 									Alexei Kovalev	 									New York Rangers
-5		 									Brian Leetch	 									New York Rangers
-6		 									Eric Lindros	 									Philadelphia Flyers
-7		 									Mike Modano	 									Dallas Stars
-8		 									Alexander Mogilny	 									Buffalo Sabres
-9		 									Jeremy Roenick	 									Chicago Blackhawks
-10		 									Alexei Yashin	 									Ottawa Senators			
+1		 			Paul Kariya	 			Anaheim Mighty Ducks
+2		 			Peter Forsberg	 			Quebec Nordiques
+3		 			Viktor Kozlov	 			San Jose Sharks
+4		 			Todd Marchant	 			Edmonton Oilers
+5		 			Oleg Tverdovsky	 			Anaheim Mighty Ducks
+6		 			Todd Harvey	 			Dallas Stars
+7		 			Kenny Jonsson	 			Toronto Maple Leafs
+8		 			Blaine Lacher	 			Boston Bruins
+9		 			Radek Bonk	 			Ottawa Senators
+10		 			Brett Lindros	 			New York Islanders
+11		 			Valeri Bure	 			Montreal Canadiens
+12		 			Brian Rolston	 			New Jersey Devils
+13		 			David Oliver	 			Edmonton Oilers
+14		 			Ian Laperriere	 			St. Louis Blues
+15		 			Adam Deadmarsh	 			Quebec Nordiques
+16		 			Pavel Bure	 			Vancouver Canucks
+17		 			Wayne Gretzky	 			Los Angeles Kings
+18		 			Jeremy Roenick	 			Chicago Blackhawks
+19		 			Dominik Hasek	 			Buffalo Sabres
+20		 			Ray Bourque	 			Boston Bruins
+21		 			Doug Gilmour	 			Toronto Maple Leafs
+22		 			Teemu Selanne	 			Winnipeg Jets
+23		 			Cam Neely	 			Boston Bruins
+24		 			Sergei Fedorov	 			Detroit Red Wings
+25		 			Bernie Nicholls	 			Chicago Blackhawks
+26		 			Jaromir Jagr	 			Pittsburgh Penguins
+27		 			Joe Sakic	 			Quebec Nordiques
+28		 			Mark Messier	 			New York Rangers
+29		 			Brett Hull	 			St. Louis Blues
+30		 			Eric Lindros	 			Philadelphia Flyers
 `
 const splitRaw = raw.split(' ')
 let triedArr = []
